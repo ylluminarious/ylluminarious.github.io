c885cebe22cd25534e857242092c0ecfab2f0396
George Plymale II <georgedp@orbitalimpact.com>
2019-04-29 14:06:00 -0400

    Site updated to 40d1e7d

--- javascripts/application.js
+++ javascripts/application.js
@@ -12,6 +12,7 @@
  * Date: 2016-01-08T19:56Z
  */
 
+
 (function( global, factory ) {
 
 	if ( typeof module === "object" && typeof module.exports === "object" ) {
@@ -11025,3 +11026,61 @@ if ( !noGlobal ) {
 
 return jQuery;
 }));
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/* globals jQuery, document */
+
+(function ($, undefined) {
+    "use strict";
+
+    var $document = $(document);
+
+    $document.ready(function () {
+
+        $(".scroll-down").arctic_scroll();
+
+        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
+            e.preventDefault();
+            $("body").toggleClass("nav-opened nav-closed");
+        });
+
+    });
+
+    // Arctic Scroll by Paul Adam Davis
+    // https://github.com/PaulAdamDavis/Arctic-Scroll
+    $.fn.arctic_scroll = function (options) {
+
+        var defaults = {
+            elem: $(this),
+            speed: 500
+        },
+
+        allOptions = $.extend(defaults, options);
+
+        allOptions.elem.click(function (event) {
+            event.preventDefault();
+            var $this = $(this),
+                $htmlBody = $('html, body'),
+                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
+                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
+                toMove;
+
+            if (offset) {
+                toMove = parseInt(offset);
+                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
+            } else if (position) {
+                toMove = parseInt(position);
+                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
+            } else {
+                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
+            }
+        });
+
+    };
+})(jQuery);
+(function() {
+
+
+}).call(this);
